46e84d6b957e99e16aff504f0f40e5fd7f5e4b73
PabloSanchi <pablo.sanchi.herrera@gmail.com>
2025-01-12 17:05:21 +0100

    chore: bump package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
 	name="light-dependency-injection",
-	version="0.1.1",
+	version="0.1.2",
 	description="A lightweight dependency injection module for Python.",
 	long_description=open("README.md").read(),
 	long_description_content_type="text/markdown",
